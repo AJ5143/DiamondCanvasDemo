8386ee52fad79193c4fc8ab7e8903b41d66104c0
Jitang Agravat <jitangagravat@gmail.com>
2021-03-01 14:14:06 +0530

    Added functionality of moving the diamond based on input co-ordinates.

--- DiamondCanvasDemo/src/application/DrawingDiamond.java
+++ DiamondCanvasDemo/src/application/DrawingDiamond.java
@@ -1,12 +1,18 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class DrawingDiamond  extends Application {
@@ -16,6 +22,34 @@ public class DrawingDiamond  extends Application {
 			double midPointsCrownTop[] = new double[4];
 			double midPointsGirdle1[] = new double[4];
 			double midPointsGirdle2[] = new double[4];
+			Label noteForMove = new Label("Note : Moving co-ordinates are relative to center");
+			Label lblXCo_Ordinate = new Label("X Co-ordinate:");
+			Label lblYCo_Ordinate = new Label("Y Co-ordinate:");
+			TextField txtX_Co_Ordinate = new TextField();
+			TextField txtY_Co_Ordinate = new TextField();
+			
+			txtX_Co_Ordinate.setPromptText("Default : 0");
+			txtY_Co_Ordinate.setPromptText("Default : 0");
+			Button btnMovePos = new Button("Move");
+			Button btnResetPos = new Button("Reset to Center");
+			HBox hbXCo = new HBox();
+			HBox hbYCo = new HBox();
+			HBox hbButtons = new HBox();
+			hbXCo.getChildren().addAll(lblXCo_Ordinate, txtX_Co_Ordinate );
+			hbYCo.getChildren().addAll(lblYCo_Ordinate, txtY_Co_Ordinate);
+			hbButtons.getChildren().addAll(btnMovePos, btnResetPos);
+			hbXCo.setAlignment(Pos.CENTER);
+			hbYCo.setAlignment(Pos.CENTER);
+			hbButtons.setAlignment(Pos.CENTER);
+			hbXCo.setSpacing(5);
+			hbYCo.setSpacing(5);
+			hbButtons.setSpacing(5);
+			
+			//double newPosX = Double.valueOf(txtX_Co_Ordinate.getText());
+			//double newPosY = Double.valueOf(txtY_Co_Ordinate.getText());
+			
+			
+			
 			gc = canvas.getGraphicsContext2D();
 			gc.setStroke(Color.CADETBLUE);
 			gc.setLineWidth(2);
@@ -51,6 +85,10 @@ public class DrawingDiamond  extends Application {
 			
 			gc.strokeLine(140, 260, 300, 435);	// Pavilion start line
 			gc.strokeLine(460, 260, 300, 435);	// Pavilion last line
+			
+			
+			//gc.strokeLine(300, 0, 300, 720);	// Vertical base line
+			//gc.strokeLine(0, 252.5, 1280, 252.5);// Horizontal base line
 			//gc.beginPath();
 			//gc.appendSVGPath("M300,435 L460,244.5,398,165.5"); // Right part of whole Diamond
 			//gc.stroke();
@@ -67,21 +105,40 @@ public class DrawingDiamond  extends Application {
 			//gc.strokeLine(420, 245, 300, 435);		// Pavilion facet 6
 			
 			
-		
-			vbCenter.setPrefSize(480,480);
-			vbCenter.getChildren().add(canvas);
-			vbCenter.setAlignment(Pos.BASELINE_CENTER);
 			
+			vbCenter.setPrefSize(800,800);
+			vbCenter.setSpacing(5);
+			vbCenter.getChildren().addAll(canvas, noteForMove , hbXCo, hbYCo, hbButtons);
+			vbCenter.setAlignment(Pos.CENTER);
+			
+			btnMovePos.setOnMouseClicked(e ->{
+				canvas.setTranslateX( Double.valueOf(txtX_Co_Ordinate.getText()));
+				canvas.setTranslateY( Double.valueOf(txtY_Co_Ordinate.getText()));
+				txtX_Co_Ordinate.clear();
+				txtY_Co_Ordinate.clear();
+			});
+			btnResetPos.setOnMouseClicked(e ->{
+				canvas.setTranslateX(0);
+				canvas.setTranslateY(0);
+				txtX_Co_Ordinate.clear();
+				txtY_Co_Ordinate.clear();
+			});
 //			scene.setOnMouseClicked(e->{
 //				vbCenter.setLayoutX(e.getSceneX());
 //				vbCenter.setLayoutY(e.getSceneY());
 //			});
-			vbCenter.setOnMouseDragged(e->{
-				vbCenter.setLayoutX(e.getSceneX() - 700);
-				vbCenter.setLayoutY(e.getSceneY() - 240);
-			});
+//			vbCenter.setOnMouseDragged(e->{
+//				vbCenter.setLayoutX(e.getSceneX() - 300);
+//				vbCenter.setLayoutY(e.getSceneY() - 240);
+//				refreshLine();
+//			});
 			//root.getChildren().add(canvas);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			System.out.println(Screen.getPrimary().getDpi());
+			System.out.println(Screen.getPrimary().getVisualBounds());
+			System.out.println(Screen.getPrimary().getBounds());
+			System.out.println(Screen.getPrimary().getOutputScaleX());
+			System.out.println(Screen.getPrimary().getOutputScaleY());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -89,6 +146,16 @@ public class DrawingDiamond  extends Application {
 		}
 	}
 	
+
+
+	private void refreshLine() {
+		Point2D horizontalFirst;
+		Point2D horizontalLast;
+		Point2D verticalFirst;
+		Point2D verticalLast;
+		
+	}
+
 
 
 	private double[] getMidPointsForTopCrown(double x1, double y1, double x2, double y2) {
@@ -109,14 +176,15 @@ public class DrawingDiamond  extends Application {
 
 
 
-	Canvas canvas = new Canvas(480,480);
+	Canvas canvas = new Canvas(500,500);
 	GraphicsContext gc; 
 	VBox vbCenter = new VBox();
 	//BorderPane root = new BorderPane();
 
-	Scene scene = new Scene(vbCenter,1280,720);
+	Scene scene = new Scene(vbCenter,1366, 768);
 
 public static void main(String[] args) {
 	launch(args);
+	//System.out.println(Screen.getPrimary());
 }
 }
