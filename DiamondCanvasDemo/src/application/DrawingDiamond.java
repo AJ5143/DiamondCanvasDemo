792eda49059a98a7e48ac2ee515aad54cc5ad609
Jitang Agravat <jitangagravat@gmail.com>
2021-03-01 18:20:35 +0530

    Got points, lines, angles inside containers for manipulation.

--- DiamondCanvasDemo/src/application/DrawingDiamond.java
+++ DiamondCanvasDemo/src/application/DrawingDiamond.java
@@ -1,5 +1,8 @@
 package application;
 
+
+import com.sun.javafx.geom.Line2D;
+
 import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
@@ -12,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.transform.Translate;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -50,19 +55,48 @@ public class DrawingDiamond  extends Application {
 			
 			
 			
-			gc = canvas.getGraphicsContext2D();
+			gc = diamond.getGraphicsContext2D();
 			gc.setStroke(Color.CADETBLUE);
 			gc.setLineWidth(2);
+			Point2D topLinePoint1 = new Point2D(203,165);
+			Point2D topLinePoint2 = new Point2D(397,165);
+			Point2D firstMiddleLinePoint1 = new Point2D(140,245);
+			Point2D firstMiddleLinePoint2 = new Point2D(460,245);
+			Point2D secondMiddleLinePoint1 = new Point2D(140,260);
+			Point2D secondMiddleLinePoint2 = new Point2D(460,260);
+			Point2D lastPointPavilionFacet = new Point2D(300,435);
+			Point2D topLineMidPoint = topLinePoint1.midpoint(topLinePoint2);
+			Point2D firstMiddleLineMidPoint = firstMiddleLinePoint1.midpoint(firstMiddleLinePoint2);
+			Point2D secondMiddleLineMidPoint = secondMiddleLinePoint1.midpoint(secondMiddleLinePoint2);
+			Line crownHeight = new Line();
+			//Line2D crownLine = new Line2D();
+			crownHeight.setStartX(topLineMidPoint.getX());
+			crownHeight.setStartY(topLineMidPoint.getY());
+			crownHeight.setEndX(firstMiddleLineMidPoint.getX());
+			crownHeight.setEndY(firstMiddleLineMidPoint.getY());
+			//gc.strokeLine(crownHeight.getStartX(), crownHeight.getStartY(), crownHeight.getEndX(), crownHeight.getEndY()); // crown height
+			
+			Line girdleHeight = new Line();
+			girdleHeight.setStartX(firstMiddleLineMidPoint.getX());
+			girdleHeight.setStartY(firstMiddleLineMidPoint.getY());
+			girdleHeight.setEndX(secondMiddleLineMidPoint.getX());
+			girdleHeight.setEndY(secondMiddleLineMidPoint.getY());
+			
+			double diameter = firstMiddleLinePoint2.getX() - firstMiddleLinePoint1.getX();
+			double crownAngle = firstMiddleLinePoint1.angle(topLinePoint1);
+			double pavilionAngle = secondMiddleLinePoint1.angle(lastPointPavilionFacet);
+			//gc.strokeLine(girdleHeight.getStartX(), girdleHeight.getStartY(), girdleHeight.getEndX(), girdleHeight.getEndY() ); // girdle height
+			
 			gc.strokeLine(203,165,397,165);		// Top part (Line at top of crown) 
-			midPointsCrownTop = getMidPointsForTopCrown(203,165,397,165);
+			midPointsCrownTop = getTwoMidPointsForLine(203,165,397,165);
 			//gc.strokeLine(midPointsTop[0], midPointsTop[1], 1, 1); // for testing only
 			//gc.strokeLine(midPointsTop[2], midPointsTop[3], 1, 1); // for testing only
 			gc.strokeLine(140,245,460,245);		// Middle part (Horizontal Line which joins crown with pavilion)
-			midPointsGirdle1 = getMidPointsForTopCrown(140,245,460,245);
+			midPointsGirdle1 = getTwoMidPointsForLine(140,245,460,245);
 			//gc.strokeLine(midPointsGirdle1[0],midPointsGirdle1[1] , 5, 5); // for testing only
 			//gc.strokeLine(midPointsGirdle1[2],midPointsGirdle1[3] , 5, 5); // for testing only
 			gc.strokeLine(140,260,460,260);		// Middle part (Second Horizontal Line which joins crown with pavilion)
-			midPointsGirdle2 = getMidPointsForTopCrown(140,260,460,260);
+			midPointsGirdle2 = getTwoMidPointsForLine(140,260,460,260);
 			//gc.strokeLine(midPointsGirdle2[0],midPointsGirdle2[1] , 5, 5); // for testing only
 			//gc.strokeLine(midPointsGirdle2[2],midPointsGirdle2[3] , 5, 5); // for testing only
 			gc.strokeLine(midPointsCrownTop[0], midPointsCrownTop[1],midPointsGirdle1[0] , midPointsGirdle1[1]); //crown facets
@@ -106,20 +140,50 @@ public class DrawingDiamond  extends Application {
 			
 			
 			
-			vbCenter.setPrefSize(800,800);
+			vbCenter.setPrefSize(700,700);
 			vbCenter.setSpacing(5);
-			vbCenter.getChildren().addAll(canvas, noteForMove , hbXCo, hbYCo, hbButtons);
-			vbCenter.setAlignment(Pos.CENTER);
+			vbCenter.getChildren().addAll(diamond, noteForMove , hbXCo, hbYCo, hbButtons);
 			
-			btnMovePos.setOnMouseClicked(e ->{
-				canvas.setTranslateX( Double.valueOf(txtX_Co_Ordinate.getText()));
-				canvas.setTranslateY( Double.valueOf(txtY_Co_Ordinate.getText()));
-				txtX_Co_Ordinate.clear();
-				txtY_Co_Ordinate.clear();
-			});
+			//vbCenter.setAlignment(Pos.BASELINE_CENTER);
+			vbCenter.setAlignment(Pos.CENTER);
+			try {
+				btnMovePos.setOnMouseClicked(e ->{
+					
+					if(txtX_Co_Ordinate.getText().isEmpty() & txtY_Co_Ordinate.getText().isEmpty()) {
+						diamond.setTranslateX(0);
+						diamond.setTranslateY(0);
+					}
+					else if(txtX_Co_Ordinate.getText().isEmpty()) {
+							diamond.setTranslateX(0);
+							diamond.setTranslateY( Double.valueOf(txtY_Co_Ordinate.getText()));
+					}
+					else if(txtY_Co_Ordinate.getText().isEmpty()){
+							diamond.setTranslateY(0);
+							diamond.setTranslateX( Double.valueOf(txtX_Co_Ordinate.getText()));
+					}
+					
+					else {
+						diamond.setTranslateX( Double.valueOf(txtX_Co_Ordinate.getText()));
+						diamond.setTranslateY( Double.valueOf(txtY_Co_Ordinate.getText()));
+					}
+					
+//					Translate t = (Translate) canvas.getTransforms();
+//					t.setX(Double.valueOf(txtX_Co_Ordinate.getText()));
+//					t.setY(Double.valueOf(txtY_Co_Ordinate.getText()));
+					//gc.translate(Double.valueOf(txtX_Co_Ordinate.getText()), Double.valueOf(txtY_Co_Ordinate.getText()));
+					txtX_Co_Ordinate.clear();
+					txtY_Co_Ordinate.clear();
+				});
+			}
+			catch(NumberFormatException e) {
+				System.out.println("Empty field not allowed");
+				
+				e.printStackTrace();
+			}
+			
 			btnResetPos.setOnMouseClicked(e ->{
-				canvas.setTranslateX(0);
-				canvas.setTranslateY(0);
+				diamond.setTranslateX(0);
+				diamond.setTranslateY(0);
 				txtX_Co_Ordinate.clear();
 				txtY_Co_Ordinate.clear();
 			});
@@ -134,11 +198,16 @@ public class DrawingDiamond  extends Application {
 //			});
 			//root.getChildren().add(canvas);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			System.out.println(Screen.getPrimary().getDpi());
-			System.out.println(Screen.getPrimary().getVisualBounds());
-			System.out.println(Screen.getPrimary().getBounds());
-			System.out.println(Screen.getPrimary().getOutputScaleX());
-			System.out.println(Screen.getPrimary().getOutputScaleY());
+			scene.setOnMouseClicked(e->{
+				diamond.setTranslateX(e.getSceneX() - 727);
+				diamond.setTranslateY(e.getSceneY() - 330);
+			});
+			
+//			System.out.println(Screen.getPrimary().getDpi());
+//			System.out.println(Screen.getPrimary().getVisualBounds());
+//			System.out.println(Screen.getPrimary().getBounds());
+//			System.out.println(Screen.getPrimary().getOutputScaleX());
+//			System.out.println(Screen.getPrimary().getOutputScaleY());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -148,17 +217,17 @@ public class DrawingDiamond  extends Application {
 	
 
 
-	private void refreshLine() {
-		Point2D horizontalFirst;
-		Point2D horizontalLast;
-		Point2D verticalFirst;
-		Point2D verticalLast;
-		
-	}
+//	private void refreshLine() {
+//		Point2D horizontalFirst;
+//		Point2D horizontalLast;
+//		Point2D verticalFirst;
+//		Point2D verticalLast;
+//		
+//	}
 
 
 
-	private double[] getMidPointsForTopCrown(double x1, double y1, double x2, double y2) {
+	private double[] getTwoMidPointsForLine(double x1, double y1, double x2, double y2) {
 		
 		double topX1;
 		double topY1;
@@ -176,9 +245,10 @@ public class DrawingDiamond  extends Application {
 
 
 
-	Canvas canvas = new Canvas(500,500);
+	Canvas diamond = new Canvas(500,500);
 	GraphicsContext gc; 
 	VBox vbCenter = new VBox();
+	
 	//BorderPane root = new BorderPane();
 
 	Scene scene = new Scene(vbCenter,1366, 768);
